e09086103ac6e7c125c4061a96ba96ac85085eb2
Hack Reactor Students <accounts@hackreactor.com>
2015-05-14 11:02:28 -0700

    Completed pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._total = 0;
+  this._counter = 0;
+  this._lineSpace = [];
+  this._storage = {};
 };
 
+Queue.prototype.enqueue = function(value){
+  this._storage[this._counter] = value;
+  this._total++;
+  this._lineSpace.push(this._counter);
+  this._counter++;
+}
+
+Queue.prototype.dequeue = function(){
+  var current = this._storage[this._lineSpace[0]];
+  delete this._storage[this._lineSpace[0]];
+  this._total--;
+  this._lineSpace.shift();
+  return current;
+}
+
+Queue.prototype.size = function(){
+  if(this._total<0){
+    return 0;
+  }
+  return this._total;
+}
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.total = 0;
 };
 
+Stack.prototype.push = function(value){
+  this.storage[this.total] = value;
+  this.total++;
+}
 
+Stack.prototype.pop = function(){
+  var current = this.storage[this.total -1];
+  delete this.storage[this.total-1];
+  this.total--;
+  return current;
+}
+
+Stack.prototype.size = function(){
+  if(this.total<0){
+    return 0;
+  }
+  return this.total;
+}
