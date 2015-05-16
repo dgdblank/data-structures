ff17e05b6b5c81381ae22481b4a704e59636e6dc
Hack Reactor Students <accounts@hackreactor.com>
2015-05-16 10:47:53 -0700

    Complete tree structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,35 @@
 var Tree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
 
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+
+  var result = false;
+  var checkChildren = function(tree){
+    if(tree.value === target){
+      return result = true;
+    } else{
+      for(var i = 0; i < tree.children.length; i++){
+          checkChildren(tree.children[i])
+     }
+   }
+  }
+
+  checkChildren(this)
+  return result;
 
 };
 
