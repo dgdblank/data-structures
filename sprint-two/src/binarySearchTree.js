10e250d66040676d02df67ecea57062bbc1f0bb8
Hack Reactor Students <accounts@hackreactor.com>
2015-05-16 11:40:59 -0700

    Progress on binary search tree insert method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,44 @@
 var BinarySearchTree = function(value){
+  this.root = value;
+  this.left = null;
+  this.right = null;
+
 };
 
 
+BinarySearchTree.prototype.insert = function(value){
+  var traverse = function(node){
+        //if our value is less than node
+    if(value<this.root){
+      if(this.left === null){
+        this.left = BinarySearchTree(value);
+      }
+      else{
+        traverse(this.left);
+      }
+      //this.left = value;
+    }
+    else{
+      if(this.right === null){
+        this.right = BinarySearchTree(value);
+      }
+      else{
+        traverse(this.right);
+      }
+    }
+    //else
+      //this.right = value;
+  }
+
+}
+
+BinarySearchTree.prototype.contains = function(value){
+
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
