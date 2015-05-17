2923bff7854bad7d81d4e629e5d061a08424249d
Hack Reactor Students <accounts@hackreactor.com>
2015-05-16 17:44:15 -0700

    Progress on graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,16 +1,30 @@
 
 
 var Graph = function(){
+  this.nodes = [];
+  this.edges = [];
 };
 
 Graph.prototype.addNode = function(node){
+  this.nodes.push(node);
 };
 
 Graph.prototype.contains = function(node){
+  for(var i = 0; i < this.nodes.length; i++){
+    return this.nodes[i] === node ? true : false;
+    }
 };
 
 Graph.prototype.removeNode = function(node){
+  var updatedNodes = [];
+  this.forEachNode(function(part){
+    if(part !== node){
+      updatedNodes.push(part);
+    }
+  });
+  this.nodes = updatedNodes;
 };
+
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
 };
@@ -22,6 +36,9 @@ Graph.prototype.removeEdge = function(fromNode, toNode){
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for(var i = 0; i < this.nodes.length; i++){
+    cb(this.nodes[i]);
+  }
 };
 
 /*
